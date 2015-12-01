2d01b4caf430095fb739c75a3b427198c67f804f
ryanbrandenburg <rybrande@microsoft.com>
2015-12-01 15:29:40 -0800

    * Missed files

---
+++ src/Microsoft.AspNet.Mvc.DataAnnotations/AttributeAdapterBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.Localization;
+
+namespace Microsoft.AspNet.Mvc.DataAnnotations
+{
+    public interface IAttributeAdapter : IClientModelValidator
+    {
+        string GetErrorMessage(ModelMetadata modelMetadata, IModelMetadataProvider metadataProvider);
+    }
+
+    public abstract class AttributeAdapterBase<TAttribute> :
+        ValidationAttributeAdapter<TAttribute>,
+        IAttributeAdapter
+        where TAttribute : ValidationAttribute
+    {
+        public AttributeAdapterBase(TAttribute attribute, IStringLocalizer stringLocalizer)
+            : base(attribute, stringLocalizer)
+        { }
+
+        public abstract string GetErrorMessage(ModelMetadata modelMetadata, IModelMetadataProvider metadataProvider);
+    }
+}
---
+++ src/Microsoft.AspNet.Mvc.DataAnnotations/ValidationAttributeAdapterTable.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.ModelBinding.Validation;
+
+namespace Microsoft.AspNet.Mvc.DataAnnotations
+{
+    public class ValidationAttributeAdapterTable
+    {
+        public static Dictionary<Type, ValidationAttributeAdapterFactory> AttributeFactories =
+            new Dictionary<Type, ValidationAttributeAdapterFactory>
+            {
+                {
+                    typeof(RegularExpressionAttribute),
+                    (attribute, stringLocalizer) => new RegularExpressionAttributeAdapter(
+                        (RegularExpressionAttribute)attribute,
+                        stringLocalizer)
+                },
+                {
+                    typeof(MaxLengthAttribute),
+                    (attribute, stringLocalizer) => new MaxLengthAttributeAdapter(
+                        (MaxLengthAttribute)attribute,
+                        stringLocalizer)
+                },
+                {
+                    typeof(MinLengthAttribute),
+                    (attribute, stringLocalizer) => new MinLengthAttributeAdapter(
+                        (MinLengthAttribute)attribute,
+                        stringLocalizer)
+                },
+                {
+                    typeof(CompareAttribute),
+                    (attribute, stringLocalizer) => new CompareAttributeAdapter(
+                        (CompareAttribute)attribute,
+                        stringLocalizer)
+                },
+                {
+                    typeof(RequiredAttribute),
+                    (attribute, stringLocalizer) => new RequiredAttributeAdapter(
+                        (RequiredAttribute)attribute,
+                        stringLocalizer)
+                },
+                {
+                    typeof(RangeAttribute),
+                    (attribute, stringLocalizer) => new RangeAttributeAdapter(
+                        (RangeAttribute)attribute,
+                        stringLocalizer)
+                },
+                {
+                    typeof(StringLengthAttribute),
+                    (attribute, stringLocalizer) => new StringLengthAttributeAdapter(
+                        (StringLengthAttribute)attribute,
+                        stringLocalizer)
+                },
+                {
+                    typeof(CreditCardAttribute),
+                    (attribute, stringLocalizer) => new DataTypeAttributeAdapter(
+                        (DataTypeAttribute)attribute,
+                        "creditcard",
+                        stringLocalizer)
+                },
+                {
+                    typeof(EmailAddressAttribute),
+                    (attribute, stringLocalizer) => new DataTypeAttributeAdapter(
+                        (DataTypeAttribute)attribute,
+                        "email",
+                        stringLocalizer)
+                },
+                {
+                    typeof(PhoneAttribute),
+                    (attribute, stringLocalizer) => new DataTypeAttributeAdapter(
+                        (DataTypeAttribute)attribute,
+                        "phone",
+                        stringLocalizer)
+                },
+                {
+                    typeof(UrlAttribute),
+                    (attribute, stringLocalizer) => new DataTypeAttributeAdapter(
+                        (DataTypeAttribute)attribute,
+                        "url",
+                        stringLocalizer)
+                }
+            };
+    };
+}
