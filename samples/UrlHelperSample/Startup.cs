d6473d4f398af22ff7d3972626369b5ea0989f1a
Kiran Challa <kichalla@microsoft.com>
2015-12-03 15:51:39 -0800

    Reacting to UrlHelperWebSite changes

--- samples/UrlHelperSample/Startup.cs
+++ samples/UrlHelperSample/Startup.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace UrlHelperSample
@@ -22,7 +22,7 @@ namespace UrlHelperSample
             // Add MVC services to the services container
             services.AddMvc();
 
-            services.AddScoped<IUrlHelper, CustomUrlHelper>();
+            services.AddSingleton<IUrlHelperFactory, CustomUrlHelperFactory>();
         }
 
         public void Configure(IApplicationBuilder app)
